e4794f7880153e215e08a9079641ac7cc116d3fb
tsaristbomba <andrexerdna@gmail.com>
2021-07-10 17:46:07 -0300

    adding netlify cms locale

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
+import "./src/styles/global.css"
 // import CMS from "netlify-cms-app"
 // import pt from "netlify-cms-locales/dist/esm/pt"
 
 // CMS.registerLocale("pt", pt)
-
-import "./src/styles/global.css"
